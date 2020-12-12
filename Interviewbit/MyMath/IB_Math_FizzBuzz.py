8f80a681d00923b6047ca6293e66ee0c51ffd699
Sujan Banerjee <banerjee.sujan@gmail.com>
2020-12-12 23:37:41 +0530

    20201220_2337_added_FizzBuzz3

--- Interviewbit/MyMath/IB_Math_FizzBuzz.py
+++ Interviewbit/MyMath/IB_Math_FizzBuzz.py
@@ -42,10 +42,25 @@ class Solution:
 
         return resArray
 
+    def fizzBuzz3(self, A):
+
+        retlist = [str(x) for x in range(1, A + 1)]
+
+        for i in range(3, A + 1, 3):
+            retlist[i - 1] = 'Fizz'
+
+        for j in range(5, A + 1, 5):
+            retlist[j - 1] = 'Buzz'
+
+        for k in range(15, A + 1, 15):
+            retlist[k - 1] = 'FizzBuzz'
+
+        return retlist
+
 
 start_time = time.perf_counter()
-A = 50000
+A = 500000
 s = Solution()
-print(s.fizzBuzz2(A))
+print(s.fizzBuzz3(A))
 end_time = time.perf_counter()
 print(end_time - start_time)   # 0.054255205  # 0.05325494
